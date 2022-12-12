cd2a93e83180236a0ff6ee800a44aad809e04fc6
roman <roman.pshenichnikov555@gmail.com>
2022-12-12 20:34:25 +0300

    Course3_Hw2

--- src/main/java/me/roman/app/controllers/FirstController.java
+++ src/main/java/me/roman/app/controllers/FirstController.java
@@ -3,17 +3,6 @@ package me.roman.app.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @RestController
 public class FirstController {
-    @GetMapping("/")
-    public String startApp() {
-        return "Приложение запущено";
-    }
-    @GetMapping("/info")
-    public String info() {
-        return "Имя: Роман,Название проекта: 'App',Дата создания проекта: '12.12.2022',Описание проекта: 'Название: App,Функции'Подсчет финансов',Пока не знаю,Java 17'";
-    }
 }
